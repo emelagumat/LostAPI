1977c1631dd59053d8c1fd6617d5309f448cff59
Manu <emelagumat@gmail.com>
2023-09-24 03:09:10 +0200

    Changed tls configuration

--- Sources/LostAPI/configure.swift
+++ Sources/LostAPI/configure.swift
@@ -5,15 +5,26 @@ import Vapor
 
 public struct LostAPIEntry {
     public static func configure(_ app: Application) async throws {
-        app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
-            hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-            port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
-            username: Environment.get("DATABASE_USERNAME") ?? "manu",
-            password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-            database: Environment.get("DATABASE_NAME") ?? "postgres",
-            tls: .prefer(try .init(configuration: .clientDefault)))
-        ), as: .psql)
-
+        if let databaseURL = Environment.get("DATABASE_URL") {
+            var tlsConfig: TLSConfiguration = .makeClientConfiguration()
+            tlsConfig.certificateVerification = .none
+            let nioSSLContext = try NIOSSLContext(configuration: tlsConfig)
+            
+            var postgresConfig = try SQLPostgresConfiguration(url: databaseURL)
+            postgresConfig.coreConfiguration.tls = .require(nioSSLContext)
+            
+            app.databases.use(.postgres(configuration: postgresConfig), as: .psql)
+        } else {
+            app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
+                hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+                port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
+                username: Environment.get("DATABASE_USERNAME") ?? "manu",
+                password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+                database: Environment.get("DATABASE_NAME") ?? "postgres",
+                tls: .prefer(try .init(configuration: .clientDefault)))
+            ), as: .psql)
+        }
+        
         try await preloadData(in: app)
         try routes(app)
     }
